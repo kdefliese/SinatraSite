5a0817ae2e8038408bef2e59ca72c959ca9d76c6
Katherine Defliese <kdefliese@users.noreply.github.com>
2015-10-27 14:07:33 -0700

    Added sinatra routes

--- my-site.rb
+++ my-site.rb
@@ -2,7 +2,19 @@ require "sinatra"
 
 class MySite < Sinatra::Base
 
-  get "/" do
+  get "/index.html" do
+    erb :index
+  end
 
+  get "/about_me.html" do
+    erb :about_me
+  end
+
+  get "/projects.html" do
+    erb :projects
+  end
+
+  get "/blog.html" do
+    erb :blog
   end
 end
